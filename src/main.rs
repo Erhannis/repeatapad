ce64e1454c00be3d13f4113b63db7003c75de61a
erhannis <eyeillus@gmail.com>
2025-08-23 20:56:55 -0400

    UNTESTED Maybe handle reads etc

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,19 @@ async fn main() {
     // main_gamepad().await;
 }
 
+const SERVICE_HOGP: Uuid = Uuid::from_short(0x1812_u16);
+const CHAR_HOGP_HID: Uuid = Uuid::from_short(0x2A4A_u16);
+const CHAR_HOGP_REPORT_MAP: Uuid = Uuid::from_short(0x2A4B_u16);
+const CHAR_HOGP_CONTROL: Uuid = Uuid::from_short(0x2A4C_u16);
+const CHAR_HOGP_PROTOCOL_MODE: Uuid = Uuid::from_short(0x2A4E_u16);
+const CHAR_HOGP_REPORT: Uuid = Uuid::from_short(0x2A4D_u16);
+
+const SERVICE_BATTERY: Uuid = Uuid::from_short(0x180F_u16);
+const CHAR_BATTERY_BATTERY: Uuid = Uuid::from_short(0x2A19_u16);
+
+const SERVICE_DEVICE_INFO: Uuid = Uuid::from_short(0x180A_u16);
+const CHAR_DEVICE_INFO_PNP: Uuid = Uuid::from_short(0x2A50_u16);
+
 async fn main_ble() {
     let (sender_tx, mut receiver_rx) = channel::<PeripheralEvent>(256);
     let mut peripheral = Peripheral::new(sender_tx).await.unwrap();
@@ -21,18 +34,100 @@ async fn main_ble() {
 
     peripheral.add_service(
         &Service {
-            uuid: Uuid::from_short(0x1812_u16), // HOGP
+            uuid: SERVICE_HOGP, // HOGP
             primary: true,
             characteristics: vec![
                 Characteristic {
-                    uuid: Uuid::from_short(0x2A4A_u16), // HID
-                    value: Some(vec![0x11, 0x01, 0x00, 0x03]), //DUMMY Not sure what these should be
+                    uuid: CHAR_HOGP_HID, // HID
                     properties: vec![CharacteristicProperty::Read],
                     ..Default::default()
                 },
                 Characteristic {
-                    uuid: Uuid::from_short(0x2A4B_u16), // report map
-                    value: Some(vec![
+                    uuid: CHAR_HOGP_REPORT_MAP, // report map
+                    properties: vec![CharacteristicProperty::Read],
+                    ..Default::default()
+                },
+                Characteristic {
+                    uuid: CHAR_HOGP_CONTROL, // control
+                    properties: vec![CharacteristicProperty::WriteWithoutResponse],
+                    ..Default::default()
+                },
+                Characteristic {
+                    uuid: CHAR_HOGP_PROTOCOL_MODE, // protocol mode
+                    properties: vec![CharacteristicProperty::Read, CharacteristicProperty::WriteWithoutResponse],
+                    ..Default::default()
+                },
+                Characteristic {
+                    uuid: CHAR_HOGP_REPORT, // report
+                    properties: vec![CharacteristicProperty::Read, CharacteristicProperty::NotifyEncryptionRequired],
+                    ..Default::default()
+                },
+            ],
+        }
+    ).await.expect("Failed to add HOGP service");
+    println!("added HOGP service");
+
+    peripheral.add_service(
+        &Service {
+            uuid: SERVICE_BATTERY, // Battery
+            primary: true,
+            characteristics: vec![
+                Characteristic {
+                    uuid: CHAR_BATTERY_BATTERY, // Battery level
+                    properties: vec![CharacteristicProperty::Read, CharacteristicProperty::Notify],
+                    ..Default::default()
+                }
+            ],
+        }
+    ).await.expect("Failed to add battery service");
+    println!("added battery service");
+
+    ////DUMMY This crashes (on my windows computer)
+    ////RAINY Make this fail-safe
+    // peripheral.add_service(
+    //     &Service {
+    //         uuid: SERVICE_DEVICE_INFO, // Device info
+    //         primary: true,
+    //         characteristics: vec![
+    //             Characteristic {
+    //                 uuid: CHAR_DEVICE_INFO_PNP, // PnP
+    //                 properties: vec![CharacteristicProperty::Read, CharacteristicProperty::Notify],
+    //                 value: Some(vec![90]),
+    //                 ..Default::default()
+    //             }
+    //         ],
+    //     }
+    // ).await.expect("Failed to add pnp service");
+    // println!("added pnp service");
+
+    peripheral.start_advertising("Repeatapad", &[ //RAINY Name of device or st
+            SERVICE_HOGP,
+            SERVICE_BATTERY,
+            // SERVICE_DEVICE_INFO,
+        ]).await.expect("Failed to advertise");
+    println!("started advertising");
+
+    while let Some(event) = receiver_rx.recv().await {
+        println!("rx event {:?}", event);
+        match event {
+            PeripheralEvent::CharacteristicSubscriptionUpdate { request, subscribed } => {
+                // Send notifications to subscribed clients
+            }
+            PeripheralEvent::ReadRequest { request, offset, responder } => {
+                let mut sent = false;
+                match request.service {
+                    SERVICE_HOGP => {
+                        match request.characteristic {
+                            CHAR_HOGP_HID => {
+                                responder.send(ReadRequestResponse {
+                                    value: vec![0x11, 0x01, 0x00, 0x03], //DUMMY Not sure what these should be
+                                    response: RequestResponse::Success,
+                                }).expect("failed send");
+                                sent = true;
+                            },
+                            CHAR_HOGP_REPORT_MAP => {
+                                responder.send(ReadRequestResponse {
+                                    value: vec![
 // HID Report Map (bytes)
 0x05, 0x01,       // Usage Page (Generic Desktop)
 0x09, 0x05,       // Usage (Game Pad)
@@ -73,95 +168,71 @@ async fn main_ble() {
 0x95, 0x04,       //   Report Count (4)
 0x81, 0x02,       //   Input (Data,Var,Abs)
 0xC0,             // End Collection
-                    ]),
-                    properties: vec![CharacteristicProperty::Read],
-                    ..Default::default()
-                },
-                Characteristic {
-                    uuid: Uuid::from_short(0x2A4C_u16), // control
-                    properties: vec![CharacteristicProperty::WriteWithoutResponse], //DUMMY Prolly needs handling
-                    ..Default::default()
-                },
-                Characteristic {
-                    uuid: Uuid::from_short(0x2A4E_u16), // protocol mode
-                    properties: vec![CharacteristicProperty::Read, CharacteristicProperty::WriteWithoutResponse],
-                    value: Some(vec![0x01]), // report mode
-                    ..Default::default()
-                },
-                Characteristic {
-                    uuid: Uuid::from_short(0x2A4D_u16), // report
-                    properties: vec![CharacteristicProperty::Read, CharacteristicProperty::NotifyEncryptionRequired],
-                    value: Some(vec![
+                                    ],
+                                    response: RequestResponse::Success,
+                                }).expect("failed send");
+                                sent = true;
+                            },
+                            CHAR_HOGP_CONTROL => { // No read, only write
+                                responder.send(ReadRequestResponse {
+                                    value: vec![],
+                                    response: RequestResponse::RequestNotSupported,
+                                }).expect("failed send");
+                                sent = true;
+                            },
+                            CHAR_HOGP_PROTOCOL_MODE => {
+                                responder.send(ReadRequestResponse {
+                                    value: vec![0x01], // report mode
+                                    response: RequestResponse::Success,
+                                }).expect("failed send");
+                                sent = true;
+                            },
+                            CHAR_HOGP_REPORT => { //DUMMY Do real report
+                                responder.send(ReadRequestResponse {
+                                    value: vec![
 0x01,   // Report ID
 0x03, 0x00,   // Buttons: 0000 0011
 0x02,      // Hat = 2 (East) in low nibble, hi nibble 0
 0x00, 0x00, 0x00, 0x00,  // X,Y,Z,Rz = 0
-                    ]),
-                    ..Default::default()
-                },
-            ],
-        }
-    ).await.expect("Failed to add HOGP service");
-    println!("added HOGP service");
-
-    peripheral.add_service(
-        &Service {
-            uuid: Uuid::from_short(0x180F_u16), // Battery
-            primary: true,
-            characteristics: vec![
-                Characteristic {
-                    uuid: Uuid::from_short(0x2A19_u16), // Battery level
-                    properties: vec![CharacteristicProperty::Read, CharacteristicProperty::Notify],
-                    value: Some(vec![90]),
-                    ..Default::default()
+                                    ],
+                                    response: RequestResponse::Success,
+                                }).expect("failed send");
+                                sent = true;
+                            },
+                        }
+                    },
+                    SERVICE_BATTERY => {
+                        match request.characteristic {
+                            CHAR_BATTERY_BATTERY => {
+                                responder.send(ReadRequestResponse {
+                                    value: vec![90], //CHECK Is this formatted right?  //RAINY Do something better with this
+                                    response: RequestResponse::Success,
+                                }).expect("failed send");
+                                sent = true;
+                            }
+                        }
+                    },
+                    SERVICE_DEVICE_INFO => {
+                        responder.send(ReadRequestResponse {
+                            value: vec![],
+                            response: RequestResponse::RequestNotSupported,
+                        }).expect("failed send");
+                        sent = true;
+                    },
+                };
+                if !sent {
+                    responder.send(ReadRequestResponse {
+                        value: vec![],
+                        response: RequestResponse::RequestNotSupported,
+                    }).expect("failed send");
                 }
-            ],
-        }
-    ).await.expect("Failed to add battery service");
-    println!("added battery service");
-
-    ////DUMMY This crashes (on my windows computer)
-    // peripheral.add_service(
-    //     &Service {
-    //         uuid: Uuid::from_short(0x180A_u16), // Device info
-    //         primary: true,
-    //         characteristics: vec![
-    //             Characteristic {
-    //                 uuid: Uuid::from_short(0x2A50_u16), // PnP
-    //                 properties: vec![CharacteristicProperty::Read, CharacteristicProperty::Notify],
-    //                 value: Some(vec![90]),
-    //                 ..Default::default()
-    //             }
-    //         ],
-    //     }
-    // ).await.expect("Failed to add pnp service");
-    // println!("added pnp service");
-
-    peripheral.start_advertising("RustBLE", &[
-            Uuid::from_short(0x1812_u16),
-            Uuid::from_short(0x180F_u16),
-            // Uuid::from_short(0x180A_u16),
-        ]).await.expect("Failed to advertise");
-    println!("started advertising");
-
-    while let Some(event) = receiver_rx.recv().await {
-        println!("rx event {:?}", event);
-        match event {
-            PeripheralEvent::CharacteristicSubscriptionUpdate { request, subscribed } => {
-                // Send notifications to subscribed clients
-            }
-            PeripheralEvent::ReadRequest { request, offset, responder } => {
-                // Respond to Read request
-                responder.send(ReadRequestResponse {
-                    value: String::from("Hello").into(),
-                    response: RequestResponse::Success,
-                }).expect("failed send");
             }
             PeripheralEvent::WriteRequest { request, offset, value, responder } => {
-                // Respond to Write request
-                responder.send(WriteRequestResponse {
-                    response: RequestResponse::Success,
-                }).expect("failed send");
+                // I don't think there's any write events we need to respond to yet
+
+                // responder.send(WriteRequestResponse {
+                //     response: RequestResponse::Success,
+                // }).expect("failed send");
             },
             _ => {}
         }
